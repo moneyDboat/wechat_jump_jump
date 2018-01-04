00261c64c3070925b8aa45c8fe2d374e9bad2917
captain <moneyboat@foxmail.com>
2018-01-04 18:02:33 +0800

    modify random swipe location

--- play.py
+++ play.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import time
+import random
 
 
 # 使用的Python库及对应版本：
@@ -19,8 +20,14 @@ def get_screenshot(id):
 def jump(distance):
     # 这个参数还需要针对屏幕分辨率进行优化
     press_time = int(distance * 1.35)
-    cmd = 'adb shell input swipe 320 410 320 410 ' + str(press_time)
+
+    # 生成随机手机屏幕模拟触摸点
+    # 模拟触摸点如果每次都是同一位置，成绩上传可能无法通过验证
+    rand = random.randint(0, 9) * 10
+    cmd = ('adb shell input swipe %i %i %i %i ' + str(press_time)) \
+          % (320 + rand, 410 + rand, 320 + rand, 410 + rand)
     os.system(cmd)
+    print(cmd)
 
 
 def get_center(img_canny, ):
